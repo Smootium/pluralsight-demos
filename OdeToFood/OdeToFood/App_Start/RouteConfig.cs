542d7a43749b653cfa157510ea647d3cd9d4840b
Gregg LaHaye <gregory.lahaye@wolterskluwer.com>
2018-12-18 13:45:25 -0500

    Added OdeToFood (MVC 4 module) that I started months ago

---
+++ OdeToFood/OdeToFood/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OdeToFood
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //order of routes matter. The routing engine will use the first route it finds that matches the request
+
+            //if 'name' was not made optional (either through name = "something" or UrlParameter.Optional), then any request that didn't specify the name would not FAD
+            //i.e., 'cuisine/french' will route to the Cuisine controller, but 'cuisine' would not
+            //Can also specify the default value in the Controller itself
+
+            //The engine will also 'find' the name in a query string, such as:
+            // /cuisine?name=italian
+            //will use 'italian' as the name in the parameter
+            routes.MapRoute(
+                name: "Cuisine",
+                url: "cuisine/{name}",
+                defaults: new { controller = "Cuisine", action = "Search", name = UrlParameter.Optional }
+                );
+
+            //This is greedy and will match just about any request
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            var message = $"{controller}::{action} {id}";
+
+            ViewBag.Message = message;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Conjuring";
+            model.Location = "Kennesaw, GA, USA";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
