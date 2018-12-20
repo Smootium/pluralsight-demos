b971d82e02fe99acd943016b91348cf578bb5981
Gregg LaHaye <gregory.lahaye@wolterskluwer.com>
2018-12-20 17:15:51 -0500

    Added an IdentityServer4 IdentityProvider. Set up some test users and identity resources

---
+++ Asp.NetCoreWithOAuth/MyCompany.IDP/Config.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace MyCompany.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "D7022502-84B8-4371-9B55-AD040580E319",
+                    Username = "George",
+                    Password = "George",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name","George"),
+                        new Claim("family_name","Monkey")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "61F635E1-40A8-413C-AD2B-334485A1D179",
+                    Username = "YellowHat",
+                    Password = "YellowHat",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name","YellowHat"),
+                        new Claim("family_name","Man")
+                    }
+                }
+            };
+        }
+
+        //IdentityResource map to scopes that give access to Identity-related info
+        //ApiResource map to scope that give access to Api resources
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                /*OpenId scope ensures the user identifier (aka, SubjectId) is included
+                  I.e., if the client requests the OpenId scope, the Subject claim is returned
+                  The profile scope maps to profile-related claims.
+                  I.e., if the client requests the Profile scope, the given_name and familiy_name claims are returned.
+                */
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            { };
+        }
+    }
+}
---
+++ Asp.NetCoreWithOAuth/MyCompany.IDP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MyCompany.IDP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+            //add '.well-known/openid-configuration' at the end of the URL to see a JSON file with the various endpoints, scopes, claims, etc.
+        }
+    }
+}
