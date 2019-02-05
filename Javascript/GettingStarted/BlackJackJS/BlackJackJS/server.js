f86ced6e7fcc31284ff6006b2f774f5132a6dbfa
Gregg LaHaye <gregory.lahaye@wolterskluwer.com>
2019-02-05 17:17:17 -0500

    Added BlackJack soln. Getting this set up to serve locally.

---
+++ Javascript/GettingStarted/BlackJackJS/BlackJackJS/server.js
@@ -0,0 +1,36 @@
+//'use strict';
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var port = process.env.PORT || 1337;
+var baseDirectory = __dirname;
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'html' });
+//    res.end('Hello World\n');
+//}).listen(port);
+http.createServer(function (request, response) {
+    try {
+        var requestUrl = url.parse(request.url)
+
+        // need to use path.normalize so people can't access directories underneath baseDirectory
+        var fsPath = baseDirectory + path.normalize(requestUrl.pathname)
+
+        var fileStream = fs.createReadStream(fsPath)
+        fileStream.pipe(response)
+        fileStream.on('open', function () {
+            response.writeHead(200)
+        })
+        fileStream.on('error', function (e) {
+            response.writeHead(404)     // assume the file doesn't exist
+            response.end()
+        })
+    } catch (e) {
+        response.writeHead(500)
+        response.end()     // end the response so browsers don't hang
+        console.log(e.stack)
+    }
+}).listen(port)
+
+console.log("listening on port " + port)
