3d2d2aaf95548c453c996d4de719dbdc28704c59
Gregg LaHaye <gregory.lahaye@wolterskluwer.com>
2019-02-11 16:09:52 -0500

    slight tweaks based on current module in course

--- Angular_DKur/APM-Start/src/app/app.component.ts
+++ Angular_DKur/APM-Start/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+//the Decorator is a function so the ( ) are necessary
+//we are passing an object to the Component function so the { } are necessary 
 @Component({
-  selector: 'pm-root',
+  selector: 'pm-root',//'selector' defines a directive (name) so it can be used/referenced in HTML (a directive is a custom HTML tag)
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
