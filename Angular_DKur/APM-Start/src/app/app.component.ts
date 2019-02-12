95421ddfc6aa90fc58f83d0bd7eb0df9ab0a161a
Gregg LaHaye <gregory.lahaye@wolterskluwer.com>
2019-02-12 17:27:23 -0500

    Installed bootstrap and font-awesome packages. Using said styles in styles.css. Added a new component (product-list). Displaying new component in app.component.

--- Angular_DKur/APM-Start/src/app/app.component.ts
+++ Angular_DKur/APM-Start/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 //we are passing an object to the Component function so the { } are necessary 
 @Component({
   selector: 'pm-root',//'selector' defines a directive (name) so it can be used/referenced in HTML (a directive is a custom HTML tag)
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //linked template
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
